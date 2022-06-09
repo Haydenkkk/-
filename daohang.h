6acd83edb7785ba570867a7bc302ec5567af2e6e
Haydenkkk <96369401+Haydenkkk@users.noreply.github.com>
2022-06-09 21:44:13 +0800

    Update daohang.h

--- daohang.h
+++ daohang.h
@@ -16,7 +16,6 @@ public:
 	void init1();//初始化，策略1，最短路径
 	void init2();//2,最短时间
 	void init3();//3，交通混合
-	void campus_maps();//地图全貌
 	int find_node(string name);//找位置
 	void Dijkstra(int start, int end,int);//Dijkstra 求最短路径，并打印出来
 	void Visiter();//游客系统
